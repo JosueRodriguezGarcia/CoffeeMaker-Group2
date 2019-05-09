4f7efe12924335e87791b5572933763f4516aba5
Josue.Rodriguez@fundacion-jala.org <Josue.Rodriguez@fundacion-jala.org>
2019-05-09 19:04:16 -0400

    GUI: most of the elements were designed

---
+++ src/model/StartButton.java
@@ -0,0 +1,15 @@
+package model;
+
+public class StartButton {
+	 private boolean isPushed;
+	
+	 public StartButton() {
+		 isPushed = false;
+	 }
+	 public boolean getStatus() {
+		 return isPushed;
+	 }
+	 public void setStatus(boolean isPushed) {
+		 
+	 }
+}
