7eff996f3b5eccfe351c4a8dba98b93b3f490df6
Alejandro Sánchez Luizaga <sanchezluizaga@gmail.com>
2019-05-10 10:11:35 -0400

    Added construction procedure example

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -5,9 +5,17 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		LightIndicator light = new LightIndicator();
+		StartButton button = new StartButton();
+		PressureValve valve = new PressureValve();
 		
+		/* You may pass as many parameters as you wish */
 		// CoffeeMaker laCafetera = new CoffeeMaker.Builder().light(light).isOn(true).build();
-		CoffeeMaker laCafetera = new CoffeeMaker.Builder().build();
+
+		/* ...or no one! */
+		// CoffeeMaker laCafetera = new CoffeeMaker.Builder().build();
+
+		/* Please do keep in mind that the parameter pass to boiler has to be an PressureValve object */
+		CoffeeMaker laCafetera = new CoffeeMaker.Builder().button(button).light(light).boiler(valve).build();
 		System.out.println("Builder: I'm done, I've just built a coffee maker for you, enjoy it!");
 		
 		laCafetera.getLightIndicator();
