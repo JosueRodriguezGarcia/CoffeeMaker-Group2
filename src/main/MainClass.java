67f2bde900c3b69005ca4461ef9ea932784e2588
Alejandro Sánchez Luizaga <sanchezluizaga@gmail.com>
2019-05-11 11:14:07 -0400

    MVC layout template in MainClass.java

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -2,8 +2,11 @@ package main;
 
 public class MainClass {
 
-
 	public static void main(String[] args) {
+
+		/* **************** */
+		/*       MODEL      */
+		/* **************** */
 		LightIndicator light = new LightIndicator();
 		StartButton button = new StartButton();
 		PressureValve valve = new PressureValve();
@@ -15,11 +18,34 @@ public class MainClass {
 		// CoffeeMaker laCafetera = new CoffeeMaker.Builder().build();
 
 		/* Please do keep in mind that the parameter passed to boiler has to be a PressureValve object */
-		CoffeeMaker laCafetera = new CoffeeMaker.Builder().button(button).light(light).boiler(valve).build();
+		CoffeeMaker model = new CoffeeMaker.Builder().button(button).light(light).boiler(valve).build();
 		System.out.println("Builder: I'm done, I've just built a coffee maker for you, enjoy it!");
+
 		
-		laCafetera.getLightIndicator();
+		
+		
+		
+		/* **************** */
+		/*       VIEW       */
+		/* **************** */
+		// Algunos pasos previos si fuere necesario
+		//CoffeeMakerGui view = new CoffeeMakerGui();
+		
+		
+		
+		
+		
+		/* **************** */
+		/*    CONTROLLER    */
+		/* **************** */
+		//CoffeeMakerController controller = new CoffeeMakerController(model, view)
+		
+		/* Aqui todo el proceso */
+		//controller.tantosTantos();   // <--- Aqui la logica: metodos de controlar que consultan y settean al model y que actualizan el estado del view
+		//controller.otrosOtros();     // <--- aqui la logica: metodos de controlar que consultan y settean al model y que actualizan el estado del view
+		//controller.unosUnos();       // <--- aqui la logica: metodos de controlar que consultan y settean al model y que actualizan el estado del view
+		//controller.pocosPocos();     // <--- aqui la logica: metodos de controlar que consultan y settean al model y que actualizan el estado del view
+		
 	}
-	
-	
+
 }
