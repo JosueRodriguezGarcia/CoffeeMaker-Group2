00e1d3221145d36b265eb3258cf1499e7187ff70
JosueRodriguezGarcia <Josue.Rodriguez@fundacion-jala.org>
2019-05-13 02:39:13 -0400

    the ckeckbox option was added to the CoffeeMakerGui.java class

--- src/view/CoffeeMakerGui.java
+++ src/view/CoffeeMakerGui.java
@@ -1,13 +1,17 @@
 package view;
 
-import java.awt.Color;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+
+import javafx.scene.image.Image;
+import view.CoffeeMakerGui.CoffeMakerEvents;
 
 public class CoffeeMakerGui extends JFrame {
-
+	
 	final String COFFEE_MAKER = "resources/coffeeMaker.png";
 	final String OPEN_VALVE = "resources/open.png";
 	final String CLOSED_VALVE = "resources/closed.png";
@@ -20,135 +24,175 @@ public class CoffeeMakerGui extends JFrame {
 	final String COFFEE = "resources/coffee.png";
 	final String COFFEE_GROUNDS = "resources/coffeeGrounds.png";
 
-	// creacion del panel que contenera todos los elementos.
-	private JPanel background;
+	private JPanel panel_1 = new JPanel();
+	private JPanel panel_2 = new JPanel();
+	
+	private JLabel title_1 = new JLabel();
+	private JLabel title_2 = new JLabel();
+	private JLabel coffeeMaker = new JLabel();
+	private JLabel valve = new JLabel();
+	private JLabel water = new JLabel();
+	private JLabel filter = new JLabel();
+	private JLabel pot = new JLabel();
+	private JLabel coffee = new JLabel();
+	private JLabel coffeeGrounds = new JLabel();
+	
+	private JButton brew = new JButton();
+	
+	private JCheckBox check_1= new JCheckBox();
+	private JCheckBox check_2= new JCheckBox();
+	private JCheckBox check_3= new JCheckBox();
+	private JCheckBox check_4= new JCheckBox();
+	
+	private CoffeeMakerGui() {
 
-	// Creacion de los diferentes Jlabel
-	private JLabel coffeeMaker;
-	private JLabel valve;
-	private JLabel water;
-	private JLabel filter;
-	private JLabel pot;
-	private JLabel coffee;
-	private JLabel coffeeGrounds;
-	// Creacion del Boton BREW
-	private JButton brew;
-
-	public CoffeeMakerGui() {
-		// Inicialiazacion del los elementos
-		background = new JPanel();
-		coffeeMaker = new JLabel();
-		valve = new JLabel();
-		water = new JLabel();
-		filter = new JLabel();
-		pot = new JLabel();
-		coffee = new JLabel();
-		coffeeGrounds = new JLabel();
-		brew = new JButton();
-
-		configCoffeeMakerGui();
-		configValve();
-		configCoffeeGrounds();
-		configFilter();
-		configPot();
-		configWater();
-		configButton();
-		configCoffee();
+		configPanel1();
+		configPanel2();
+		configJFrame();
 		coffeeMakerEvents();
-		configCoffeeMaker();
-
 	}
 
-	public void configCoffeeMakerGui() {
-		setTitle("COFFEE MAKER");
-		setLayout(null);
-		setBounds(100, 100, 700, 520);
-		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	private void configPanel1() {
 
-	}
-
-	public void configJPanel() {
-		background = new JPanel();
-		background.setBounds(0, 0, 638, 520);
-		add(background);
-	}
-
-	public void configCoffeeMaker() {
-		coffeeMaker.setIcon(new ImageIcon(COFFEE_MAKER));
-		coffeeMaker.setBounds(0, 0, 608, 472);
-		add(coffeeMaker);
-	}
-
-	public void configValve() {
-		valve.setIcon(new ImageIcon(OPEN_VALVE));
-		valve.setBounds(315, 85, 61, 35);
-		add(valve);
-	}
-
-	public void configFilter() {
-		filter.setIcon(new ImageIcon(FILTER));
-		filter.setBounds(395, 157, 299, 59);
-		add(filter);
-	}
-
-	public void configPot() {
-		pot.setIcon(new ImageIcon(POT));
-		pot.setBounds(330, 223, 250, 185);
-		add(pot);
-	}
-
-	public void configWater() {
+		//panel_1.setBackground(Color.blue);
+		panel_1.setBounds(0, 0, 700, 520);
+		panel_1.setLayout(null);
+		
+		
+		//coffeeGrounds.setIcon(new ImageIcon(COFFEE_GROUNDS));
+		coffeeGrounds.setBounds(399, 170, 89, 33);
+		panel_1.add(coffeeGrounds);
+		
+		//coffee.setIcon(new ImageIcon(COFFEE));
+		coffee.setBounds(341, 283, 201, 117);
+		panel_1.add(coffee);
+		
 		water.setIcon(new ImageIcon(NOT_WATER));
 		water.setBounds(31, 93, 212, 297);
-		add(water);
-	}
-
-	public void configCoffee() {
-		coffee.setIcon(new ImageIcon(COFFEE));
-		coffee.setBounds(341, 283, 201, 117);
-		add(coffee);
-	}
-
-	public void configCoffeeGrounds() {
-		coffeeGrounds.setIcon(new ImageIcon(COFFEE_GROUNDS));
-		coffeeGrounds.setBounds(399, 170, 89, 33);
-		add(coffeeGrounds);
-	}
-
-	public void configButton() {
+		panel_1.add(water);
+		
+		//pot.setIcon(new ImageIcon(POT));
+		pot.setBounds(330, 223, 250, 185);
+		panel_1.add(pot);
+		
+		//filter.setIcon(new ImageIcon(FILTER));
+		filter.setBounds(395, 157, 299, 59);
+		panel_1.add(filter);
+		
+		valve.setIcon(new ImageIcon(OPEN_VALVE));
+		valve.setBounds(315, 85, 61, 35);
+		panel_1.add(valve);
+		
+		coffeeMaker.setIcon(new ImageIcon(COFFEE_MAKER));
+		coffeeMaker.setBounds(0, 0, 608, 472);
+		panel_1.add(coffeeMaker);
+		
 		brew.setText("BREW");
 		brew.setFocusPainted(false);
 		brew.setBackground(Color.LIGHT_GRAY);
 		brew.setBounds(210, 425, 111, 35);
-		add(brew);
+		panel_1.add(brew);
+		
+		add(panel_1);
 	}
 
-	public void placePot() {
-		pot.setIcon(new ImageIcon(POT));
+	private void configPanel2() {
+
+		panel_2.setBackground(Color.yellow);
+		panel_2.setBounds(700, 120, 200, 200);
+		panel_2.setLayout(null);
+		
+		title_2.setText("LIST COMPONENT");
+		title_2.setHorizontalAlignment(SwingConstants.CENTER);
+		title_2.setBounds(0, 0, 200, 30);
+		panel_2.add(title_2);
+		
+		check_1.setText("FILTER");
+		check_1.setBackground(Color.yellow);
+		check_1.setBounds(0, 50, 200, 30);
+		check_2.setText("VALVE");
+		check_2.setBackground(Color.yellow);
+		check_2.setBounds(0, 80, 200, 30);
+		check_3.setText("POT");
+		check_3.setBackground(Color.yellow);
+		check_3.setBounds(0, 110, 200, 30);
+		check_4.setText("WATER");
+		check_4.setBackground(Color.yellow);
+		check_4.setBounds(0, 140, 200, 30);
+		
+		panel_2.add(check_1);
+		panel_2.add(check_2);
+		panel_2.add(check_3);
+		panel_2.add(check_4);
+
 	}
 
+	private void configJFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 900, 520);
+		setTitle("COFFEE MAKER");
+		setLayout(null);
+		setVisible(true);
+		
+		add(panel_2);
+
+	}
 	public void coffeeMakerEvents() {
 		CoffeMakerEvents events = new CoffeMakerEvents();
+		//brew.setBackground(Color.BLACK);
 		brew.addActionListener(events);
+		check_1.addActionListener(events);
+		check_2.addActionListener(events);
+		check_3.addActionListener(events);
+		check_4.addActionListener(events);
 	}
-
 	public static void main(String[] args) {
 		new CoffeeMakerGui().setVisible(true);
 	}
-
 	class CoffeMakerEvents implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			if (e.getSource() == brew) {
-				// pot.setIcon(new ImageIcon(POT_NOT_EMPTY));
 				if (brew.getBackground() == Color.LIGHT_GRAY) {
-					// placePot();
+					coffee.setIcon(new ImageIcon(COFFEE));
 					brew.setBackground(Color.GREEN);
-					// configPot();
 				} else {
 					brew.setBackground(Color.LIGHT_GRAY);
 				}
 			}
+			if(e.getSource() == check_1) {
+				if(check_1.isSelected()) {
+					filter.setIcon(new ImageIcon(FILTER));
+				}else {
+					filter.setIcon(new ImageIcon());
+				}
+				
+			}
+			if(e.getSource() == check_2) {
+				if(check_2.isSelected()) {
+					valve.setIcon(new ImageIcon(CLOSED_VALVE));
+				}else {
+					valve.setIcon(new ImageIcon(OPEN_VALVE));
+				}
+				
+			}
+			if(e.getSource() == check_3) {
+				if(check_3.isSelected()) {
+					pot.setIcon(new ImageIcon(POT_EMPTY));
+				}else {
+					pot.setIcon(new ImageIcon());
+				}
+				
+			}
+			if(e.getSource() == check_4) {
+				if(check_4.isSelected()) {
+					water.setIcon(new ImageIcon(WATER));
+				}else {
+					water.setIcon(new ImageIcon(NOT_WATER));
+				}
+				
+			}
+			
 		}
 	}
 }
+
